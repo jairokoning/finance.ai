dfdde12b0b68490d81a4a2b94042192c383dd664
Jairo Koning <jairo.koning@gmail.com>
2024-11-07 20:04:07 -0300

    feat(web): add clerk auth

--- apps/web/app/(pages)/login/page.tsx
+++ apps/web/app/(pages)/login/page.tsx
@@ -2,12 +2,15 @@ import Image from 'next/image'
 import { Button } from '../../_components/ui/button'
 import { LogInIcon } from 'lucide-react'
 import { redirect } from 'next/navigation'
+import { SignInButton } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
 
 const LoginPage = async () => {
-  // const { userId } = await auth();
-  // if (userId) {
-  //   redirect("/");
-  // }
+  const { userId } = await auth()
+  if (userId) {
+    redirect('/')
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
@@ -25,12 +28,12 @@ const LoginPage = async () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        {/* <SignInButton> */}
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
-        {/* </SignInButton> */}
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       {/* DIREITA */}
       <div className="relative h-full w-full">
