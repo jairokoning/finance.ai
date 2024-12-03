2b6d6353b263b31cbca60da43c80e9ae7a19452b
Jairo Koning <jairo.koning@gmail.com>
2024-12-03 16:48:51 -0300

    fix(web): add scroll to dashboard components

--- apps/web/app/(pages)/(home)/page.tsx
+++ apps/web/app/(pages)/(home)/page.tsx
@@ -27,7 +27,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   return (
     <>
       <Navbar />
-      <div className="p-6 space-y-6">
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
